71ae3f17609500d694ccb1da3a3c7d62c18fe74a
Tyler McCarthy <tweedlitweedli@gmail.com>
2021-04-23 23:39:11 -0500

    tiles adding

--- src/PlayState.hx
+++ src/PlayState.hx
@@ -1,17 +1,42 @@
 package;
 
+import data.Levels;
 import flixel.FlxG;
 import flixel.FlxState;
-import flixel.system.scaleModes.PixelPerfectScaleMode;
+import flixel.tile.FlxTilemap;
+import flixel.tile.FlxBaseTilemap.FlxTilemapAutoTiling;
+
+typedef LevelItem = {
+    var item:Int;
+}
 
 class PlayState extends FlxState {
+    static inline final TILE_SIZE = 16;
+
     override public function create() {
         super.create();
 
-        camera.pixelPerfectRender = true;
+        // camera.pixelPerfectRender = true;
+
+        createTilemap(Levels.data);
     }
 
     override public function update(elapsed:Float) {
         super.update(elapsed);
+    }
+
+    function createTilemap (data:Array<Array<LevelItem>>) {
+        var tileArray = [];
+
+        for (y in 0...data.length) {
+            for (x in 0...data[y].length) {
+                tileArray.push(data[y][x].item);
+            }
+        }
+
+        var tileMap = new FlxTilemap();
+        tileMap.loadMapFromArray(tileArray, 15, 10, AssetPaths.tiles__png, TILE_SIZE, TILE_SIZE, FlxTilemapAutoTiling.OFF, 1, 1, 1);
+        tileMap.useScaleHack = false;
+        add(tileMap);
     }
 }
---
+++ src/data/Levels.hx
@@ -0,0 +1,16 @@
+package data;
+
+class Levels {
+    public static final data = [
+        [{item: 1},{item: 2},{item: 2},{item: 2},{item: 2},{item: 2},{item: 2},{item: 2},{item: 2},{item: 2},{item: 2},{item: 2},{item: 2},{item: 2},{item: 3}],
+        [{item: 5},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 7}],
+        [{item: 5},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 7}],
+        [{item: 5},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 7}],
+        [{item: 5},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 7}],
+        [{item: 5},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 7}],
+        [{item: 5},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 7}],
+        [{item: 5},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 7}],
+        [{item: 5},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 6},{item: 7}],
+        [{item: 9},{item: 10},{item: 10},{item: 10},{item: 10},{item: 10},{item: 10},{item: 10},{item: 10},{item: 10},{item: 10},{item: 10},{item: 10},{item: 10},{item: 11}]
+    ];
+}
